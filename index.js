ec19d966426e2acb4524fe44d0921b0d72a2b24a
HarukiShimizu1012 <a5821039@aoyama.jp>
2022-06-10 17:48:54 +0900

    commit

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ function onTaskTitleClicked(task) {
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
     const currentValue = task.isDone;
-    
-    currentValue = task.isDone;
+
+    task.isDone =! currentValue;
 
     return task;
 }
